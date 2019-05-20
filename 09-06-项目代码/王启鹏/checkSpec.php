c2ea6bdc126660fa8e6949ca6d463039946a843c
wqp <861026685@qq.com>
2019-05-20 15:56:03 +0800

    修改API

--- 09-06-项目代码/王启鹏/checkSpec.php
+++ 09-06-项目代码/王启鹏/checkSpec.php
@@ -1,9 +1,10 @@
 <?php
 
-//$PID=$_POST['SID'];
-//$UID=$_POST['UID'];
-$SID = 1;
-$UID = 1;
+$SID=$_POST['SID'];
+$UID=$_POST['UID'];
+
+// $SID=2;
+
 $con=@new mysqli("123.206.68.192", "mysqluser", "16211621");
 //如果连接错误
 if (mysqli_connect_errno()) {
@@ -26,8 +27,8 @@ if($runSQL){
 
 //cited, workNum
 $sql=("select sum(cited) as cited, count(*) as workNum from specialist, specialist_achievement, achievement, paper ".
-    "where specialist.id=specialist_achievement.sid and specialist_achievement.aid=achievement.id and achievement.id=paper.id ".
-    "group by specialist.id having specialist.id='$SID'");
+    "where specialist.id=specialist_achievement.sid and specialist_achievement.aid=achievement.id and achievement.id=paper.id and specialist.id='$SID'".
+    "group by specialist.id ");
 $runSQL=mysqli_query($con, $sql);
 if($runSQL){
     while($row = mysqli_fetch_assoc($runSQL)){
@@ -42,11 +43,17 @@ $sql=("select count(*) as journalNum from specialist, specialist_achievement, ac
     "group by specialist.id ");
 $runSQL=mysqli_query($con, $sql);
 if($runSQL){
+    $ok=0;
     while($row = mysqli_fetch_assoc($runSQL)){
+        $ok=1;
         array_push($data, $row);
     }
+    if($ok==0){
+        array_push($data, array("journalNum"=>"0"));
+    }
+} else {
+    array_push($data, array("journalNum"=>"0"));
 }
-
 
 //sessionNum
 $sql=("select count(*) as sessionNum from specialist, specialist_achievement, achievement, paper ".
@@ -54,9 +61,16 @@ $sql=("select count(*) as sessionNum from specialist, specialist_achievement, ac
     "group by specialist.id ");
 $runSQL=mysqli_query($con, $sql);
 if($runSQL){
+    $ok=0;
     while($row = mysqli_fetch_assoc($runSQL)){
+        $ok=1;
         array_push($data, $row);
     }
+    if($ok==0) {
+        array_push($data, array("sessionNum"=>"0"));
+    }
+} else {
+    array_push($data, array("sessionNum"=>"0"));
 }
 
 //zhuanzhuNum
@@ -65,9 +79,16 @@ $sql=("select count(*) as zhuanzhuNum from specialist, specialist_achievement, a
     "group by specialist.id ");
 $runSQL=mysqli_query($con, $sql);
 if($runSQL){
+    $ok=0;
     while($row = mysqli_fetch_assoc($runSQL)){
+        $ok=1;
         array_push($data, $row);
     }
+    if($ok==0){
+        array_push($data, array("zhuanzhuNum"=>"0"));
+    }
+} else {
+    array_push($data, array("zhuanzhuNum"=>"0"));
 }
 
 //elseNum
@@ -76,11 +97,22 @@ $sql=("select count(*) as elseNum from specialist, specialist_achievement, achie
     "group by specialist.id ");
 $runSQL=mysqli_query($con, $sql);
 if($runSQL){
+    $ok=0;
     while($row = mysqli_fetch_assoc($runSQL)){
+        $ok=1;
         array_push($data, $row);
     }
+    if($ok==0){
+        array_push($data, array("elseNum"=>"0"));
+    }
+} else {
+    array_push($data, array("elseNum"=>"0"));
 }
-echo $data;
-//$json = json_encode($data);
+
+$json = json_encode($data);
+echo($json);
+//var_dump($json);
+//echo($json);
+
 //$utf8 = @preg_replace("/\\\u([0-9a-f]{4})/ie", "iconv('UTF-16BE', 'UTF-8', pack('H4','\\1'))", $json);
 //echo $utf8;
