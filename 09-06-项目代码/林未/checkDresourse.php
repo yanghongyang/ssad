b87e707edc7665e9933969ce8afa69925839da63
abc123000111 <lin_444@live.com>
2019-05-21 19:53:49 +0800

    update

--- 09-06-项目代码/林未/checkDresourse.php
+++ 09-06-项目代码/林未/checkDresourse.php
@@ -13,13 +13,12 @@ if(mysqli_connect_errno()){
 mysqli_set_charset($con,'utf8');
 mysqli_select_db($con, "test");
 
-$sqlcheck = ("select url as DresourceURL, title, paper.date as time, cited as citedNum from download, achievement, paper where resource = achievement.id and achievement.id = paper.id and downloader = '$uid';" );
+$sqlcheck = ("select resource, download.time as time, title, author, paper.date as date, cited from download, achievement, paper where resource = achievement.id and achievement.id = paper.id and downloader = '$uid';" );
 $runcheck = mysqli_query($con, $sqlcheck);
 $data = array();
 while ($row = mysqli_fetch_assoc($runcheck)) {
     $data[] = $row;
 }
 $json = json_encode($data);
-$utf8 = @preg_replace("/\\\u([0-9a-f]{4})/ie", "iconv('UTF-16BE', 'UTF-8', pack('H4','\\1'))", $json);
-echo $utf8;
+echo $json;
 ?>
