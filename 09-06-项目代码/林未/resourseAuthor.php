55d1f632d1c937b3dfa92a106323531cc4a83b15
abc123000111 <lin_444@live.com>
2019-05-21 20:56:55 +0800

    up

--- 09-06-项目代码/林未/resourseAuthor.php
+++ 09-06-项目代码/林未/resourseAuthor.php
@@ -6,20 +6,19 @@ $pid = $_POST['paperID'];
 $con=@new mysqli("123.206.68.192", "mysqluser", "16211621");
 //如果连接错误
 if(mysqli_connect_errno()){
-    echo 3; //数据库连接失败
+    echo "数据库连接失败"; //数据库连接失败
     $con=null;
     exit;
 }
 mysqli_set_charset($con,'utf8');
 mysqli_select_db($con, "test");
 
-$sqlcheck = ("select sid, name, specialist_achievement.order from specialist_achievement, specialist where sid = id and aid = '$pid';" );
+$sqlcheck = ("select sid, name, specialist_achievement.order from specialist_achievement, specialist where sid = id and aid = '$pid' order by specialist_achievement.order;" );
 $runcheck = mysqli_query($con, $sqlcheck);
 $data = array();
 while ($row = mysqli_fetch_assoc($runcheck)) {
     $data[] = $row;
 }
 $json = json_encode($data);
-$utf8 = @preg_replace("/\\\u([0-9a-f]{4})/ie", "iconv('UTF-16BE', 'UTF-8', pack('H4','\\1'))", $json);
-echo $utf8;
+echo $json;
 ?>
