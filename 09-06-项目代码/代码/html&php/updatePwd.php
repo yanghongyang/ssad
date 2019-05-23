bfe52e116cee96db190b2fc4d83dc633912a4855
MCA <mca@MCAdeMacBook-Pro.local>
2019-05-23 19:16:49 +0800

    修改个人信息的网页和更改密码的php

---
+++ 09-06-项目代码/代码/html&php/updatePwd.php
@@ -0,0 +1,28 @@
+﻿<?php
+$uid = $_POST['id'];
+$pass = $_POST['pass'];
+
+//$uid = 3;
+//$pass = "hhhhh";
+
+$con=@new mysqli("123.206.68.192", "mysqluser", "16211621");
+//如果连接错误
+if(mysqli_connect_errno()){
+    echo 2; //数据库连接失败
+    $con=null;
+    exit;
+}
+mysqli_set_charset($con,'utf8');
+mysqli_select_db($con, "test");
+
+$sql = ("update user set password = '$pass' where id = '$uid';" );
+$run = mysqli_query($con, $sql);
+if($run == TRUE)
+{
+    echo 0; //重置成功
+}
+else
+{
+    echo 1; //修改数据库失败
+}
+?>
