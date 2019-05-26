f0410da554d9aea44ce85510135c4d8b3c31adf2
abc123000111 <abc123000111@abc123000111deMacBook-Pro.local>
2019-05-26 22:58:14 +0800

    login

--- 09-06-项目代码/林未/login.php
+++ 09-06-项目代码/林未/login.php
@@ -2,25 +2,31 @@
 $uname = $_POST['nickname'];
 $pass = $_POST['password'];
 
-//$uname = "abc123000111";
-//$pass = "123456";
+//$uname = "abc12300011";
+//$pass = "12356";
 
 $con=@new mysqli("123.206.68.192", "mysqluser", "16211621");
 //如果连接错误
 if(mysqli_connect_errno()){
-    echo 5; //数据库连接失败
+    $data['result'] = 5;
+    $json = json_encode($data);
+    echo $json; //数据库连接失败
     $con=null;
     exit;
 }
 mysqli_set_charset($con,'utf8');
 mysqli_select_db($con, "test");
 
+$data = array();
+
 $sqlcheck = ("select count(*) from user where nickname = '$uname';" );
 $runcheck = mysqli_query($con, $sqlcheck);
 $resultcheck = mysqli_fetch_array($runcheck);
 if($resultcheck[0] == 0)
 {
-    echo 1; //该用户不存在
+    $data['result'] = 1;
+    $json = json_encode($data);
+    echo $json; //该用户不存在
 }
 else
 {
@@ -29,7 +35,9 @@ else
     $result1 = mysqli_fetch_array($run1);
     if($result1[0] == 0)
     {
-        echo 2; //用户密码不正确
+        $data['result'] = 2;
+        $json = json_encode($data);
+        echo $json; //用户密码不正确
     }
     else
     {
@@ -38,7 +46,9 @@ else
         $result2 = mysqli_fetch_array($run2);
         if($result2[0] == 1)
         {
-            echo 3; //该用户已登录
+            $data['result'] = 3;
+            $json = json_encode($data);
+            echo $json; //该用户已登录
         }
         else
         {
@@ -46,11 +56,18 @@ else
             $run3 = mysqli_query($con, $sql3);
             if($run3 == TRUE)
             {
-                echo 0; //登录成功
+                $sql4 = ("select id, avator, type from user where nickname = '$uname' and password = '$pass';");
+                $run4 = mysqli_query($con, $sql4);
+                $data = mysqli_fetch_assoc($run4);
+                $data['result'] = 0;
+                $json = json_encode($data);
+                echo $json;
             }
             else
             {
-                echo 4; //更新数据库失败
+                $data['result'] = 4;
+                $json = json_encode($data);
+                echo $json; //更新数据库失败
             }
         }
     }
